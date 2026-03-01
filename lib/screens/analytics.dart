573735c72197c7cd96de74d736d0057d0a9fea5e
Pinitha Savidya <savidyabalasooriya617@gmail.com>
2026-03-01 10:54:52 +0530

    Add app bar for new screens

--- lib/screens/analytics.dart
+++ lib/screens/analytics.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../configs/size_config.dart';
+
 class Analytics extends StatefulWidget {
   const Analytics({super.key});
 
@@ -10,6 +12,29 @@ class Analytics extends StatefulWidget {
 class _AnalyticsState extends State<Analytics> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: SizeConfig.blockWidth * 5),
+              child: Row(
+                children: [
+                  IconButton(onPressed: () {} ,
+                      icon: Icon(Icons.arrow_back_ios)),
+
+                  SizedBox(width: SizeConfig.blockWidth * 16,),
+
+                  Text("Analytics",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: SizeConfig.blockWidth * 5
+                    ),),
+                ],
+              ),
+            )],
+        ),),
+    );
   }
 }
