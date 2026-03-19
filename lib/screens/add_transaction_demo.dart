db4b3a440a37cf762ba1dbeace2c4e236568192b
Pinitha Savidya <savidyabalasooriya617@gmail.com>
2026-03-19 20:03:27 +0530

    Add text transform field

--- lib/screens/add_transaction_demo.dart
+++ lib/screens/add_transaction_demo.dart
@@ -11,8 +11,19 @@ class AddTransactionDemo extends StatefulWidget {
 }
 
 class _AddTransactionState extends State<AddTransactionDemo> {
-  String firstName = 'NAme';
-  final nameController = TextEditingController();
+  String firstName = 'Dummy';
+  bool showPassword = false;
+  final firstNameController = TextEditingController();
+  final lastNameController = TextEditingController();
+  final passwordController = TextEditingController();
+  final emailController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -59,21 +70,63 @@ class _AddTransactionState extends State<AddTransactionDemo> {
               padding: const EdgeInsets.all(16.0),
               child: Column(
                 children: [
-                  TextField(
-                    controller: nameController,
-                    decoration: InputDecoration(
-                      labelText: 'First Name',
-                      hintText: 'Please enter your name',
-                      border: UnderlineInputBorder(),
-                      prefixIcon: Icon(Icons.person),
-                    ),
-                    onChanged: (value) => {
-                      setState(() {
-                        firstName = value;
-                      })
-                    },
+                  Form(
+                      child: Column(
+                    children: [
+                      TextFormField(
+                        controller: firstNameController,
+                        decoration: InputDecoration(
+                          labelText: 'First Name',
+                          hintText: 'Please enter your name',
+                          border: UnderlineInputBorder(),
+                          prefixIcon: Icon(Icons.person),
+                        ),
+                        keyboardType: TextInputType.text,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "First name is required";
+                          }
+                        },
+                      ),
+                      TextFormField(
+                        controller: lastNameController,
+                        decoration: InputDecoration(
+                          labelText: 'Last Name',
+                          hintText: 'Please enter your last name',
+                          border: UnderlineInputBorder(),
+                          prefixIcon: Icon(Icons.person),
+                        ),
+                        keyboardType: TextInputType.text,
+                      ),
+                      TextFormField(
+                        // maxLines: 5,
+                        controller: passwordController,
+                        decoration: InputDecoration(
+                            labelText: 'Password',
+                            hintText: 'Please enter your name',
+                            border: UnderlineInputBorder(),
+                            prefixIcon: Icon(Icons.key),
+                            suffixIcon: IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    showPassword = !showPassword;
+                                  });
+                                },
+                                icon: Icon(Icons.remove_red_eye))),
+                        keyboardType: TextInputType.text,
+                        obscureText: !showPassword, //hide text like password
+                        // onChanged: (value) => {
+                        //   setState(() {
+                        //   })
+                        // },
+                      ),
+                      TextFormField()
+                    ],
+                  )),
+                  TextField(),
+                  SizedBox(
+                    height: 10,
                   ),
-                  SizedBox(height: 10,),
                   Text("First Name : $firstName")
                 ],
               ),
@@ -82,5 +135,15 @@ class _AddTransactionState extends State<AddTransactionDemo> {
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    firstNameController.dispose();
+    lastNameController.dispose();
+    passwordController.dispose();
+    emailController.dispose();
+
+    super.dispose();
   }
 }
