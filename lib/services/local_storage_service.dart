bb03e16c68d63dc277a39f48c6f2a37b89f85b25
Pinitha Savidya <savidyabalasooriya617@gmail.com>
2026-05-03 09:10:44 +0530

    Add analytics and profile screen with mock data

--- lib/services/local_storage_service.dart
+++ lib/services/local_storage_service.dart
@@ -6,7 +6,7 @@ class LocalStorageService{
 
   Future<bool> getIsFirstTime() async {
     final prefs = await SharedPreferences.getInstance();
-    return prefs.getBool(isFirstTime) ?? false;
+    return prefs.getBool(isFirstTime) ?? true;
   }
 
   Future<bool> setIsFirstTime() async {
